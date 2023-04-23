d02c3e836fe0d95ffd39ebe29f723eea2b8bdd7c
Bradley <bradley.t.sheehan@gmail.com>
2023-04-23 12:05:51 -0600

    add localhost to config
    will need to edit testnet priv key after starting up a testnet instance with 'npx hardhat node'

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,11 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.18",
    networks: {
+    localhost: {
+      url:"http://localhost:8545",
+      chainId:31337,
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
+    },
     shibuya: {
       url: "https://evm.shibuya.astar.network",
       chainId:81,
