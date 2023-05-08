912db7164731c137a5c1d99c2d79f8c7ba6318ec
bakon <bakon@bakon.dev>
2023-05-08 14:02:24 -0500

    dummy PK in accounts array under config

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,13 +13,13 @@ module.exports = {
     shibuya: {
       url: "https://evm.shibuya.astar.network",
       chainId:81,
-      accounts: [process.env.DEPLOY_KEY],
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
       gas: 0x5B8D80
     },
     astar: {
       url: "https://evm.astar.network",
       chainId:592,
-      accounts: [process.env.DEPLOY_KEY],
+      accounts: ["0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"],
       gas: 0x5B8D80
     }
   }
